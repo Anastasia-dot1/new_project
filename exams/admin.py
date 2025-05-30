9935a1b6377919a71fe495b32b9cb050cf1f88ca
Анастасия <chursina_anastasia@MacBook-Air-Anastasia.local>
2025-05-30 11:32:23 +0300

    Настроена админка для caexam

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import caexam
+from django.contrib.auth.models import User
 
-# Register your models here.
+class caexamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'exam_date', 'is_public')
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('title', 'users__email')
+    filter_horizontal = ('users',)  # для удобного редактирования M2M
+    
+    date_hierarchy = 'exam_date'  # иерархия по дате экзамена
+
+admin.site.register(caexam, caexamAdmin)

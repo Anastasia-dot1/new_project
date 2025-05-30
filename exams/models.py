51c1aea09a1c355efa54b171231b244740c58d40
Анастасия <chursina_anastasia@MacBook-Air-Anastasia.local>
2025-05-30 11:29:15 +0300

    Добавлена модель caexam

--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class caexam(models.Model): 
+    title = models.CharField(max_length=200, verbose_name="Название экзамена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    exam_date = models.DateTimeField(verbose_name="Дата проведения экзамена")
+    image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания", blank=True, null=True)
+    users = models.ManyToManyField(User, verbose_name="Пользователи")
+    is_public = models.BooleanField(default=False, verbose_name="Опубликовано")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Экзамен"
+        verbose_name_plural = "Экзамены"
